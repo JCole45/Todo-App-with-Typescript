23380bef333aa420207895c5027295aa75e62ab0
John <john.coleman951@gmail.com>
2020-02-04 13:35:55 +0000

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 interface Todos {
   mytodos: string;
   id: number;
-  status: string;
+  completed: boolean;
 }
 
 interface AddTodo {
@@ -30,12 +30,22 @@ function App () {
     setTodos([...todos, {
       mytodos: mytodo.here,
       id: Math.floor(Math.random() * 300),
-      status: 'uncompleted'
-    }])
+      completed: false,
+    }]);
+
+    addTodo({here:''})
+
   }
 
   const handleAdd = (e: any) => {
     addTodo({here: e.target.value})
+
+  }
+
+  const deleteTodo = (e: number) => {
+    setTodos(todos.filter(i=> i.id !==e ))
+    console.log(e)
+    console.log(todos)
   }
 
   return (
@@ -46,7 +56,7 @@ function App () {
       <button onClick={() => handleChange()}>Add Todo</button>
       
       {todos.map(i => 
-        <div key={i.id}>{i.mytodos} <button onClick={()=>{setEditID({editid: i.id}); console.log(edittodo.editid); todos.filter(j=> j.id !== edittodo.editid)}}>X</button> </div>)}
+        <div key={i.id}>{i.mytodos} <button onClick={()=>{deleteTodo(i.id); }}>X</button> </div>)}
 
       </header>
     </div>
