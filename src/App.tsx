e553f192120b02cb036c9db66b5d3ea91926c8f8
John <john.coleman951@gmail.com>
2020-02-04 12:32:30 +0000

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
+interface Todos {
+  mytodos: string;
+  id: number;
+  status: string;
+}
+
+interface AddTodo {
+  here: string;
+}
+
+interface editID {
+  editid: number;
+}
+
+//const [todos , setTodos] = useState<Todos >({mytodos: 'here', id:0, status:'completed'})
+
+
+function App () {
+
+  const [todos , setTodos] = useState<Array<any>>([])
+  const [mytodo, addTodo] = useState<AddTodo >({here: ''})
+  const [edittodo, setEditID] = useState<editID>({editid: 0})
+
+  const handleChange = () => {
+    console.log('activated')
+    setTodos([...todos, {
+      mytodos: mytodo.here,
+      id: Math.floor(Math.random() * 300),
+      status: 'uncompleted'
+    }])
+  }
+
+  const handleAdd = (e: any) => {
+    addTodo({here: e.target.value})
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3>Todo App</h3>
+      <input value={mytodo.here} onChange={handleAdd}></input>
+      <button onClick={() => handleChange()}>Add Todo</button>
+      
+      {todos.map(i => 
+        <div key={i.id}>{i.mytodos} <button onClick={()=>{setEditID({editid: i.id}); console.log(edittodo.editid); todos.filter(j=> j.id !== edittodo.editid)}}>X</button> </div>)}
+
       </header>
     </div>
   );
